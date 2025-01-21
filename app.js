0e31ced4f85c24329c67adea237b16594ee19b83
Vamberto <vambertosilva.vas@gmail.com>
2025-01-20 22:28:00 -0300

    finished

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function listarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.textContent = "";
+  amigos.forEach((amigo) => {
+    let item = document.createElement("li");
+    item.textContent = amigo;
+    lista.appendChild(item);
+  });
+}
+
+function adicionarAmigo() {
+  let nome = document.getElementById("amigo").value.trim();
+  if (nome === "") {
+    alert("Digite um nome válido!");
+    return;
+  }
+  amigos.push(nome);
+  listarAmigos();
+  document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo() {
+  if (amigos.length >= 2) {
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = `O amigo sorteado foi: ${sorteado}`;
+  } else {
+    alert("Adicione pelo menos 2 amigos para sortear!");
+  }
+}
